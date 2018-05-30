e58f4c5f49fb928b15a23647b82a6a7ff38746c8
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-05-30 01:08:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays(string) {
  return ('Happy holidays!');
 }
 
-function happyHolidaysTo(name){
+function happyHolidaysTo(string){
 return string.toName('Happy holidays, ${name}!');
 }
 
